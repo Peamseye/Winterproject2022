f03c5dcf197121d8d56dd4948379b74c7c982910
Peamseye <aispotato587@gmail.com>
2022-02-08 14:06:52 +0100

    aln

--- Winterproject2022/Program.cs
+++ Winterproject2022/Program.cs
@@ -4,10 +4,15 @@ using Raylib_cs;
 Raylib.InitWindow(800, 600, "Winter project: Raylib");
 Raylib.SetTargetFPS(60);
 
+
+Rectangle playerRect = new Rectangle();
+
 while (!Raylib.WindowShouldClose())
 {
 
     Raylib.BeginDrawing();
+
+    Raylib.DrawRectangle(10, 10, 32, 32, Color.WHITE);
 
     Raylib.ClearBackground(Color.BLUE);
 
